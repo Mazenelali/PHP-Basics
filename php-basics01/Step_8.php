e04602d9115ad3ebb7feb680181b21af170ef736
Mazen El Ali <mazenelali>
2023-02-17 20:32:19 +0200

    step 8

---
+++ php-basics01/Step_8.php
@@ -0,0 +1,9 @@
+<?php 
+$item = array(
+    array( 'balade', 'mazere3' ),
+    array('Fresh', 'Taanayel'),
+    array('Tanoureen', 'Reem')
+);
+
+echo "Hey Sir, Please I need 1 pack of ".$item[0][0]. " eggs and 3 ".$item[2][1]." Water Pack."
+?>
