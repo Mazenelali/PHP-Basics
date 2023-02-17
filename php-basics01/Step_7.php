17812532fdbb7abc81cf0216444bd4d2521ebb85
Mazen El Ali <mazenelali>
2023-02-17 20:12:05 +0200

    step 7

---
+++ php-basics01/Step_7.php
@@ -0,0 +1,12 @@
+<?php
+$birth_year = 2000;
+$current_year = 2023;
+
+$old_year = $current_year-$birth_year;
+
+if ($old_year > 18 ){
+   echo "you can drive";
+}else {
+    echo "you still a kid , go and play GTA";
+}
+?>
