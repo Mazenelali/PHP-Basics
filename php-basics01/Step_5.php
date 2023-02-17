5c64f4a4e7780e764bb7d49e3806d8b29d2a6e1e
Mazen El Ali <mazenelali>
2023-02-17 19:57:13 +0200

    step 5

---
+++ php-basics01/Step_5.php
@@ -0,0 +1,15 @@
+<?php
+$course_name= "php";
+$enrolled_students = 50;
+$price = 15.55;
+$on_discount = true;
+
+
+ echo nl2br(
+    "course title : $course_name
+    enrolled students : $enrolled_students
+    course price :$price USD
+    course on discout : $on_discount
+
+")
+?>
