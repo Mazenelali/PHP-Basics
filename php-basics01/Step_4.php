57b9ba48832bffd4d4aff4073a3f637431a65336
Mazen El Ali <mazenelali>
2023-02-17 19:28:59 +0200

    step 4

---
+++ php-basics01/Step_4.php
@@ -0,0 +1,14 @@
+<?php
+$name = "mazen";
+$position = "student";
+$github_url = "https://github.com/Mazenelali/PHP-Basics/tree/main/php-basics01";
+//method 1
+ echo "hello,I'm ".$name ."i'm a ". $position ."my github is :". $github_url;
+ echo "<br>";
+//method 2
+ echo "hello,I'm $name i'm a  $position my github is : $github_url";
+ echo "<br>";
+//method 3
+echo 'hello,I\'m '.$name. 'i\'m a ' .$position.' my github is : '.$github_url;
+
+?>
