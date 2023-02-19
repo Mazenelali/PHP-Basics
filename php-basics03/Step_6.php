e5489590b29fb621d03e913991d051d099aa24e1
Mazen El Ali <mazenelali>
2023-02-20 01:43:26 +0200

    step 6

---
+++ php-basics03/Step_6.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i = 0 ; $i <= 5 ; $i++){
+    $repeat = str_repeat("*",$i);
+    echo $repeat."<br>";
+}
+for ($i = 5 ; $i >= 0 ; $i--){
+    $repeat = str_repeat("*",$i);
+    echo $repeat."<br>";
+}
+
+
+?>
