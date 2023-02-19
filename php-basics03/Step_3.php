74416dc8892e76a74f464e23c2d53c34cf4fa8da
Mazen El Ali <mazenelali>
2023-02-20 01:05:22 +0200

    fix step 3

--- php-basics03/Step_3.php
+++ php-basics03/Step_3.php
@@ -4,13 +4,14 @@ function countWord($text ,$word ){
 $var = count(explode(" ",$text));
 $array = explode(" ",$text);
 $i=0;
+$j=0;
 while ($i<= $var){
     if($array[$i] == $word ){
-        echo $word." ";
+        $j=$j+1;
         
     }
     $i++;
-}
+}echo $j;
 }
 echo countWord("hello hello hello mhamd hello elali","hello")
 
